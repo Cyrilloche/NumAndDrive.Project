6d5504bad981cccefb21406d74ef7b62e1eab067
Cyrilloche <cyril.cherrier.dev@gmail.com>
2024-04-29 13:14:57 +0200

    Create ViewModel for register an user

---
+++ NumAndDrive/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NumAndDrive.ViewModels.Account
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Firstname { get; set; } = string.Empty;
+
+        [Required]
+        public string Lastname { get; set; } = string.Empty;
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+
+    }
+}
