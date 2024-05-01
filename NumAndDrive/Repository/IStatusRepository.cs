e692ec4677262e7f40954762fb33e0aec9021f14
Cyrilloche <cyril.cherrier.dev@gmail.com>
2024-05-01 18:40:42 +0200

    Create repository folder and StatusRepository

---
+++ NumAndDrive/Repository/IStatusRepository.cs
@@ -0,0 +1,11 @@
+﻿using NumAndDrive.Models;
+
+namespace NumAndDrive.Repository
+{
+    public interface IStatusRepository
+    {
+        Task<List<Status>> GetAllStatusesAsync();
+        Task<Status> GetStatusByIdAsync(int statusId);
+        Task<Status> GetStatusByUserIdAsync(int userId);
+    }
+}
---
+++ NumAndDrive/Repository/StatusRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NumAndDrive.Database;
+using NumAndDrive.Models;
+using NumAndDrive.Repository;
+
+namespace NumAndDrive.Services
+{
+    public class StatusRepository : IStatusRepository
+    {
+        private readonly NumAndDriveContext _context;
+        public StatusRepository(NumAndDriveContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Status>> GetAllStatusesAsync()
+        {
+            return await _context.Statuses.ToListAsync();
+        }
+
+        public async Task<Status> GetStatusByIdAsync(int statusId)
+        {
+            return await _context.Statuses.FirstOrDefaultAsync(s => s.StatusId == statusId);
+        }
+
+    }
+}
