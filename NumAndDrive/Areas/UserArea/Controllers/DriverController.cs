30087f9afe87ba382247f6e0d547a2c2db4fefd0
Cyrilloche <cyril.cherrier.dev@gmail.com>
2024-05-07 16:55:10 +0200

    Start to create the CreateTravelPage

---
+++ NumAndDrive/Areas/UserArea/Controllers/DriverController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NumAndDrive.Areas.UserArea.Controllers
+{
+    [Area("UserArea")]
+    [Authorize]
+    public class DriverController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateTravel()
+        {
+            return View();
+        }
+    }
+}
