4ef02b23d067d89aee6cdbc50364b5ff5f13ae2a
Cyrilloche <cyril.cherrier.dev@gmail.com>
2024-05-21 14:57:02 +0200

    Passenger home page

---
+++ NumAndDrive/Areas/UserArea/Controllers/PassengerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NumAndDrive.Areas.UserArea.Controllers
+{
+    [Area("UserArea")]
+    [Authorize]
+    public class PassengerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
