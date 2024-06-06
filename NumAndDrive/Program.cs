e1772190268b8e830eb5525d02f03d467af152fa
Cyrilloche <cyril.cherrier.dev@gmail.com>
2024-06-06 09:46:46 +0200

    Display Error on login page in case on connexion failure

--- NumAndDrive/Program.cs
+++ NumAndDrive/Program.cs
@@ -124,6 +124,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Urls.Add("http://0.0.0.0:80");
+//app.Urls.Add("http://0.0.0.0:80");
 
 app.Run();
